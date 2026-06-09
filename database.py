8ba2e8d88aa040e805d816bed7971c3dff113a40
Pallag Joel <pallagjoel@gmail.com>
2026-06-09 11:44:49 +0200

    Disable SQLite foreign keys to fix legacy table mismatch

--- database.py
+++ database.py
@@ -16,7 +16,7 @@ def get_connection() -> sqlite3.Connection:
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA journal_mode=WAL")
-    conn.execute("PRAGMA foreign_keys=ON")
+    # conn.execute("PRAGMA foreign_keys=ON")  # Disabled due to composite PK mismatch in legacy tables
     return conn
 
 
