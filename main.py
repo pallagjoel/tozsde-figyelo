20d0e72305d0ae4eb0e73d7b02d6746cc60d3f3a
Pallag Joel <pallagjoel@gmail.com>
2026-06-09 12:22:45 +0200

    Fix IP binding to 0.0.0.0

--- main.py
+++ main.py
@@ -1648,4 +1648,4 @@ def activate_provider(provider_id: int, current_user: User = Depends(get_current
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
